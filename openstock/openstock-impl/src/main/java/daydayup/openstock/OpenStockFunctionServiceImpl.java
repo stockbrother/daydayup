ffe8794216e0331f79569b0d96f28df5366c1f87
wu <wu@WINDOWS-5H1GI79>
2017-06-03 14:32:36 +0800

    a

--- openstock/openstock-impl/src/main/java/daydayup/openstock/OpenStockFunctionServiceImpl.java
+++ openstock/openstock-impl/src/main/java/daydayup/openstock/OpenStockFunctionServiceImpl.java
@@ -5,11 +5,14 @@ import org.slf4j.LoggerFactory;
 
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.container.XNamed;
+import com.sun.star.frame.XController;
 import com.sun.star.frame.XDesktop;
 import com.sun.star.frame.XModel;
 import com.sun.star.lang.XComponent;
 import com.sun.star.lang.XServiceInfo;
 import com.sun.star.sheet.XSheetCellCursor;
+import com.sun.star.sheet.XSheetCellRange;
+import com.sun.star.sheet.XSheetCellRanges;
 import com.sun.star.sheet.XSpreadsheet;
 import com.sun.star.sheet.XSpreadsheetDocument;
 import com.sun.star.sheet.XSpreadsheetView;
@@ -66,24 +69,34 @@ public class OpenStockFunctionServiceImpl extends com.sun.star.lib.uno.helper.We
 	@Override
 	public int osFirst(XPropertySet arg0) {
 		try {
+			// https://wiki.openoffice.org/wiki/Documentation/DevGuide/OfficeDev/Using_the_Desktop
 			Object desktop = this.xContext.getServiceManager().createInstanceWithContext("com.sun.star.frame.Desktop",
 					this.xContext);
 			XDesktop xDesktop = UnoRuntime.queryInterface(XDesktop.class, desktop);
 			XComponent xComp = xDesktop.getCurrentComponent();
-			
+
 			XInterface xDoc = UnoRuntime.queryInterface(XInterface.class, xComp);
 			XSpreadsheetDocument xDoc2 = UnoRuntime.queryInterface(XSpreadsheetDocument.class, xDoc);
-			
+
 			XModel xModel = UnoRuntime.queryInterface(XModel.class, xComp);
+			XController xControl = xModel.getCurrentController();
+			Object viewData = xControl.getViewData();
+
 			XSpreadsheetView xView = (XSpreadsheetView) UnoRuntime.queryInterface(XSpreadsheetView.class,
 					xModel.getCurrentController());
-			
+
 			XSpreadsheet xSheet = xView.getActiveSheet();
+
 			XNamed xName = UnoRuntime.queryInterface(XNamed.class, xSheet);
 			XSheetCellCursor xCursor = UnoRuntime.queryInterface(XSheetCellCursor.class, xSheet);
-			
+			Object selected = xModel.getCurrentSelection();
+			XSheetCellRange xSelected2 = UnoRuntime.queryInterface(XSheetCellRange.class, selected);
+			XSheetCellRanges xSelected = UnoRuntime.queryInterface(XSheetCellRanges.class, selected);
+
 			if (LOG.isTraceEnabled()) {
-				LOG.trace("" + xSheet + ",xName:" + xName.getName());
+				LOG.trace("" + xSheet + ",xName:" + xName.getName() + ",currentSelection:" + selected + ",xSelected:"
+						+ xSelected + ",xSelected2:" + xSelected2 + ",viewData.class:" + viewData.getClass()
+						+ ",viewData:" + viewData);
 			}
 		} catch (Exception e) {
 			throw new RuntimeException(e);
