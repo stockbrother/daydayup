52d93228d4275ef742db0cb952b4b878430297d6
wuzhen <wuzhen0808@163.com>
2017-07-28 17:27:14 +0800

    a

---
+++ openstock/openstock-impl/src/main/java/daydayup/openstock/xueqiu/XueqiuDataCollector.java
@@ -0,0 +1,13 @@
+package daydayup.openstock.xueqiu;
+
+/**
+ * http://api.xueqiu.com/stock/f10/balsheet.csv?symbol=SH601166&page=1&size=10000
+ * http://api.xueqiu.com/stock/f10/incstatement.csv?symbol=SH601166&page=1&size=10000
+ * http://api.xueqiu.com/stock/f10/cfstatement.csv?symbol=SH601166&page=1&size=10000
+ * 
+ * @author wuzhen
+ *
+ */
+public class XueqiuDataCollector {
+
+}
