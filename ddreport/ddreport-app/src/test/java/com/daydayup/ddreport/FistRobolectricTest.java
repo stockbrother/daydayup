113abc8eb80c166f283d4d193a43cdbd1c23a0df
Wu <wuzhen0808@163.com>
2017-08-09 21:19:47 +0800

    fix robolectric test:upgrade to android gradle plugin 3.0.0-a9

--- ddreport/ddreport-app/src/test/java/com/daydayup/ddreport/FistRobolectricTest.java
+++ ddreport/ddreport-app/src/test/java/com/daydayup/ddreport/FistRobolectricTest.java
@@ -6,12 +6,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(manifest=Config.NONE)
+//@Config(manifest=Config.NONE)
 public class FistRobolectricTest {
     @Rule
     public MockitoRule rule = MockitoJUnit.rule();
@@ -29,7 +30,7 @@ public class FistRobolectricTest {
         System.out.println("tested");
         Context context = this.getContext();
 
-        //MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+        TmpActivity activity = Robolectric.setupActivity(TmpActivity.class);
         //activity.findViewById(R.id.action_search).performClick();
 
         //Intent expectedIntent = new Intent(activity, SearchActivity.class);
